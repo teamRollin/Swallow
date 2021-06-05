957c68d326f0069840754db787dbde2aa59592be
HYEBIN,PARK <hyeqls3742@gmail.com>
2021-06-05 19:19:36 +0900

    Complete Course 2

---
+++ src/components/Router.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
+
+const AppRouter = ({isLoggedIn}) => {
+    return (
+        <Router>
+            {isLoggedIn && <Navigation/>}
+            <Switch>
+                {
+                    isLoggedIn
+                        ? (
+                            <> < Route exact path = "/" > <Home/>
+                        </Route>
+                        <Route exact="exact" path="/profile">
+                            <Profile/>
+                        </Route>
+                    </>
+                        )
+                        : (
+                            <> < Route exact path = "/" > <Auth/>
+                        </Route>
+                    </>
+                        )
+                }
+            </Switch>
+        </Router>
+    );
+};
+export default AppRouter;
