97d02c5235883708cd220dcff6353e15fa6cb043
ICCHOI <cicblack@ks.ac.kr>
2021-06-06 19:46:09 +0900

    feat: add File Upload feature

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,20 @@ import {authService} from "fbase";
 
 function App() {
     const [init, setInit] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    useEffect(()=> {
-      authService.onAuthStateChanged((user) => {
-        if(user){
-          setIsLoggedIn(true);
-        } else{
-          setIsLoggedIn(false);
-        }
-        setInit(true);
-      });
-    }, [])
-    return (
-      <> 
-        {init ? < AppRouter isLoggedIn = { isLoggedIn } /> : "Initializing...."}
-        <footer>&copy; {new Date().getFullYear()} Swallow</footer>
-      </>
+    const [userObj, setUserObj] = useState(null);
+    useEffect(() => {
+        authService.onAuthStateChanged((user) => {
+            if (user) {
+                setUserObj(user);
+            }
+            setInit(true);
+        });
+    }, []);
+    return (<> {
+        init ? ( <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj}/>)
+          : ("Initializing....")} 
+            <footer>&copy; {new Date().getFullYear()} Swallow</footer> 
+    </>
     );
 }
 
